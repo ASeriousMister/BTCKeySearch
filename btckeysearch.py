4081d0f50d52a375d56ad57983aece61197782b2
MisterSerious <43838750+ASeriousMister@users.noreply.github.com>
2022-07-28 16:31:04 +0000

    Update btckeysearch.py
    Added support for BIP32 root keys and XMR private spend keys

--- btckeysearch.py
+++ btckeysearch.py
@@ -4,16 +4,29 @@ import os
 import argparse
 import re
 
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
 # intro
-print('\n*=*=*= BTC Key Search =*=*=*\n')
+print(color.YELLOW + '\n*=*=*= BTC Key Search =*=*=*\n' + color.END)
 print('This tool scans through all the files in a given directory (including subdirectories)')
 print('searching for strings that could be private keys.')
-print('To run it use the -d option and provide the directory you want to scan.')
-print('example: btckeysearch.py /home/user/dir\n\n')
+print('To run it use the -d option and provide the directory you want to scan.\n')
+print(color.RED + 'example:' + color.END + ' btckeysearch.py -d /home/user/dir\n')
 
-parser = argparse.ArgumentParser(description='Bitcoin private key finder')
-parser.add_argument('-d', metavar='directory', type=str,
-                    required=True, help='Directory to scan')
+parser = argparse.ArgumentParser(description='Cryptocurrency private key finder')
+parser.add_argument('-d', metavar='directory', type=str, required=True, help='Directory to scan')
 args = parser.parse_args()
 directory = args.d
 
@@ -41,13 +54,14 @@ listOfFiles = getListOfFiles(directory)
 os.chdir(directory)
 
 n_files = len(listOfFiles)
-print(f'there are {n_files} files to scan')
+print(color.GREEN + f'There are {n_files} files to scan' + color.END)
 i = 0
+matches = []    # bitcoin private keys
+matchexp = []   # xpriv keys
+matchex = []    # monero private spend key
 # iterate through all the files
 while i < n_files:
     f = open(listOfFiles[i], 'r')
-#    filetext = f.read()
-#    f.close()
     prog = 1
 # iterate through the lines in the current file
     while True:
@@ -55,11 +69,54 @@ while i < n_files:
         if not line:
             break
 # regex for Bitcoin private keys
-        matches = re.findall(r'[5KL][1-9A-HJ-NP-Za-km-z]{50,51}$', line)
-        if matches != [] and prog == 1:
-            print(f'\n======== Matches in {listOfFiles[i]} ========')
-        if matches != []:
-            print(f'{prog}:  {matches}')
-            prog += 1
+        res = re.findall(r'[5KL][1-9A-HJ-NP-Za-km-z]{50,51}$', line)
+        if res:
+            matches.append(res)
+# regex for xpriv keys
+        res2 = re.findall(r'\bxprv[a-km-zA-HJ-NP-Z1-9]{107,108}\b', line)
+        if res2:
+            matchexp.append(res2)
+# regex for xmr private spend keys
+        res3 = re.findall(r'\b[0-9a-fA-F]{62}[0][0-9a-fA-F]\b', line)
+        if res3:
+            matchex.append(res3)
     f.close()
+# print matches in the current file
+# bitcoin private keys
+    if len(matches) > 0:
+        prog = 0
+        print(color.DARKCYAN + f'\n======== Bitcoin private keys in {listOfFiles[i]} ========' + color.END)
+        nk = len(matches)
+        iw = 0
+        while iw < nk:
+            if matches[iw]:
+                prog += 1
+                print(f'{prog}: {matches[iw]}')
+            iw += 1
+        matches = []
+# xpriv keys
+    if len(matchexp) > 0:
+        prog2 = 0
+        print(color.DARKCYAN + f'\n======== BIP32 rootkeys in {listOfFiles[i]} ========' + color.END)
+        nk2 = len(matchexp)
+        iw2 = 0
+        while iw2 < nk2:
+            if matchexp[iw2]:
+                prog2 += 1
+                print(f'{prog2}: {matchexp[iw2]}')
+            iw2 += 1
+        matchexp = []
+# monero private spend key
+    if len(matchex) > 0:
+        print(color.DARKCYAN + f'\n======== Monero private spend keys in {listOfFiles[i]} ========' + color.END)
+        nk3 = len(matchex)
+        iw3 = 0
+        prog3 = 0
+        while iw3 < nk3:
+            if matchex[iw3]:
+                prog3 += 1
+                print(f'{prog3}: {matchex[iw3]}')
+            iw3 += 1
+        matchex = []
+# go to next file
     i += 1
